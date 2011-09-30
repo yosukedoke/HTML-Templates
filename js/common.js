e6669b6945c9da92ec707d15b04fe7380f928543
yd_niku <yd.niku@gmail.com>
2011-09-30 19:15:42 +0900

    edit

---
+++ js/common.js
@@ -0,0 +1,18 @@
+﻿(function(global){
+	var dispatcher = global.dispatcher || (global.dispatcher = function dispatcher(path, func) {
+	  dispatcher.path_func = dispatcher.path_func || []
+	  if (func) return dispatcher.path_func.push([path, func]);
+	  for(var i = 0, l = dispatcher.path_func.length; i < l; ++i) {
+		var func = dispatcher.path_func[i];
+		var match = path.match(func[0]);
+		match && func[1](match);
+	  };
+	});
+	
+	//dispatcher(location.pathname);
+})(this);
+
+// Common initialization when dom ready
+$(function(){
+	
+});
