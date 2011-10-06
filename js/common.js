3085eed276eda424baff8afdf77bc4f916199ec9
yd_niku <yd.niku@gmail.com>
2011-10-06 18:56:09 +0900

    js調整

--- js/common.js
+++ js/common.js
@@ -1,5 +1,58 @@
 ﻿(function(global){
-	var dispatcher = global.dispatcher || (global.dispatcher = function dispatcher(path, func) {
+	/** Browser Check Patttern **/
+	/*
+	$.browser.webkit; //(as of jQuery 1.4)
+	$.browser.safari; //(deprecated)
+	$.browser.opera;
+	$.browser.msie;
+	$.browser.mozilla;
+	
+	$.browser.ipad = navigator.platform == 'iPad';
+	*/
+	
+	/** Debug mode check Pattern **/
+	var DomainName = global.DomainName || (global.DomainName = {});
+	DomainName.env = location.host.indexOf('example.com') >= 0 ? 'prod' : 'dev';
+	
+	/** Debug Pattern **/
+	(function(){
+		if ($.browser.ipad) return;
+		if (!global.console) global.console = {};
+		var names = [
+			"log", "debug", "info", "warn", "error", "assert", "dir", "dirxml",
+			"group", "groupEnd", "time", "timeEnd", "count", "trace", "profile", "profileEnd"
+		];
+		for (var i = 0, n; n = names[i]; i++) {
+			if (typeof(global.console[n]) != 'function') {
+				global.console[n] = function() {};
+			}
+		}
+	})();
+	global.debug || (global.debug = {
+		log: function(label) {
+			if (DomainName.env == 'prod') return;
+			var args = Array.prototype.slice.call(arguments, 1);
+			if (!/String/.test(Object.prototype.toString.call(label))) {
+				args.unshift(label);
+				label = 'DomainName DEBUG';
+			}
+			if ($.browser.ipad) {
+				console.log(label+ ': ' + args.join(','));
+			}
+			else {
+				args.unshift("[" + label + "]");
+				if ($.browser.msie) {
+					console.log(args);
+				}
+				else {
+					console.log.apply(console, args);
+				}
+			}
+		}
+	});
+	
+	/** Routing Pattern **/
+	global.dispatcher || (global.dispatcher = function dispatcher(path, func) {
 	  dispatcher.path_func = dispatcher.path_func || []
 	  if (func) return dispatcher.path_func.push([path, func]);
 	  for(var i = 0, l = dispatcher.path_func.length; i < l; ++i) {
@@ -14,5 +67,7 @@
 
 // Common initialization when dom ready
 $(function(){
-	
+	$('#container').css({
+		'opacity':'1'
+	});
 });
